bfb50f29717e7ac43b2ed0a514f981492d27dde1
faizasidd <faiza.siddiqi1@gmail.com>
2022-03-31 23:08:38 -0400

    Created HTML elements and comment cards in JS

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -21,4 +21,51 @@ const commentsArray = [
     }
 ];
 
+//Creates HTML comment cards based off the above array
+
+const displayComment = () => {
+    const commentContainer = document.querySelector('.comments__list');
+    commentContainer.innerHTML = ''; // wipe all comments and regenerate
+
+    for (let i = commentsArray.length - 1; i >= 0; i--) {
+        const author = createElement('div',
+            'comment__author', commentsArray[i].author);
+        const timestamp = createElement('h3',
+            'comment__timestamp', commentsArray[i].timestamp);
+        const text = createElement('p',
+            'comment__text', commentsArray[i].text);
+        const img = createElement('img',
+            'comment__img', '', commentsArray[i].img);
+
+        const commentCard = createElement('div', 'comment');
+
+        commentCard.appendChild(img);
+        commentCard.appendChild(author);
+        commentCard.appendChild(timestamp);
+        commentCard.appendChild(text);
+
+        commentContainer.appendChild(commentCard);
+    }
+}
+
+//  creates HTML element with classname, content and src
+
+const createElement = (elementType, className, content = '', src = '') => {
+    let element = elementType === 'img' && !src
+        ? document.createElement('div') // If img src is empty then create a div with same class (grey background)
+        : document.createElement(elementType);
+    let classList = className.split(' ');
+    classList.forEach(className => {
+        element.classList.add(className);
+    });
+
+    if (elementType === 'img' && src) {
+        element.setAttribute('src', src);
+    } else {
+        element.innerText = content;
+    }
+
+    return element;
+}
+
 
