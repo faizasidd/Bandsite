a07d905afe7772c7f87707a499e16eb7bce963c5
faizasidd <faiza.siddiqi1@gmail.com>
2022-04-17 18:42:48 -0400

    Quick bug fix from sprint 1 feedback

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -5,116 +5,102 @@ const apiKey = "33c092c8-f84f-497b-9e06-b1f2c281ccdf";
 const displayShows = () => {
     const showContainer = document.querySelector('.shows__list');
 
-// Labels
+    // Labels
 
-      const labels = document.createElement('div');
-      labels.classList.add(['labels']);
-      dateLabel = document.createElement('h3');
-      dateLabel.classList.add(['show-card__labels']);
-      dateLabel.innerText = 'DATE';
-      venueLabel = document.createElement('h3');
-      venueLabel.classList.add(['show-card__labels'], ['show-card__labels--venue']);
-      venueLabel.innerText = 'VENUE';
-      locationLabel = document.createElement('h3');
-      locationLabel.classList.add(['show-card__labels'], ['show-card__labels--location']);
-      locationLabel.innerText = "LOCATION";
-  
-      labels.appendChild(dateLabel);
-      labels.appendChild(venueLabel);
-      labels.appendChild(locationLabel);
-  
-      showContainer.appendChild(labels);
-    
-// Function
-
-axios.get(`https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`)
-.then(response => {
-    console.log(response.data);
-    response.data.forEach((show) => {
+    const labels = document.createElement('div');
+    labels.classList.add(['labels']);
     dateLabel = document.createElement('h3');
-    dateLabel.classList.add(['show-card__label']);
-    dateLabel.innerText = 'Date';
-     venueLabel = document.createElement('h3');
-     venueLabel.classList.add(['show-card__label'], ['show-card__label--venue']);
-     venueLabel.innerText = 'Venue';
-     locationLabel = document.createElement('h3');
-     locationLabel.classList.add(['show-card__label'], ['show-card__label--location']);
-     locationLabel.innerText = 'Location';
-  
-// Show date in a Weekday Month Date Year format
+    dateLabel.classList.add(['show-card__labels']);
+    dateLabel.innerText = 'DATE';
+    venueLabel = document.createElement('h3');
+    venueLabel.classList.add(['show-card__labels'], ['show-card__labels-venue']);
+    venueLabel.innerText = 'VENUE';
+    locationLabel = document.createElement('h3');
+    locationLabel.classList.add(['show-card__labels'], ['show-card__labels-location']);
+    locationLabel.innerText = "LOCATION";
 
-const numDate = parseInt(show.date);
-const newDate = new Date(numDate);
-const weekday = newDate.toLocaleString("en-ca", {weekday: "short"});
-const month = newDate.toLocaleString("en-ca", {month: "short"});
-const day = newDate.getDate();
-const year = newDate.getFullYear();
-const dateOne = weekday + ' ' + month +  ' ' + day + ' ' + year;
+    labels.appendChild(dateLabel);
+    labels.appendChild(venueLabel);
+    labels.appendChild(locationLabel);
+
+    showContainer.appendChild(labels);
+
+    // Function
+
+    axios.get(`https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`)
+        .then(response => {
+            console.log(response.data);
+            response.data.forEach((show) => {
+                dateLabel = document.createElement('h3');
+                dateLabel.classList.add(['show-card__label']);
+                dateLabel.innerText = 'Date';
+                venueLabel = document.createElement('h3');
+                venueLabel.classList.add(['show-card__label'], ['show-card__label-venue']);
+                venueLabel.innerText = 'Venue';
+                locationLabel = document.createElement('h3');
+                locationLabel.classList.add(['show-card__label'], ['show-card__label-location']);
+                locationLabel.innerText = 'Location';
+
+                // Show date in a Weekday Month Date Year format
+
+                const numDate = parseInt(show.date);
+                const newDate = new Date(numDate);
+                const weekday = newDate.toLocaleString("en-ca", { weekday: "short" });
+                const month = newDate.toLocaleString("en-ca", { month: "short" });
+                const day = newDate.getDate();
+                const year = newDate.getFullYear();
+                const dateOne = weekday + ' ' + month + ' ' + day + ' ' + year;
 
 
-// Create date, venue and location class
+                // Create date, venue and location class
 
-const date = document.createElement('h3');
-date.classList.add(['show-card__date']);
-date.innerText = dateOne;
-const place = document.createElement('p');
-place.classList.add(['show-card__text']);
-place.innerText = show.place;
-const location = document.createElement('p');
-location.classList.add(['show-card__text']);
-location.innerText = show.location;
-const button = document.createElement('button');
-button.classList.add(['btn'], ['btn--ticket']);
-button.innerText = 'Buy Tickets';
+                const date = document.createElement('h3');
+                date.classList.add(['show-card__date']);
+                date.innerText = dateOne;
+                const place = document.createElement('p');
+                place.classList.add(['show-card__text']);
+                place.innerText = show.place;
+                const location = document.createElement('p');
+                location.classList.add(['show-card__text']);
+                location.innerText = show.location;
+                const button = document.createElement('button');
+                button.classList.add(['btn'], ['btn--ticket']);
+                button.innerText = 'Buy Tickets';
 
-const showCard = document.createElement('div');
-showCard.classList.add('show-card');
-showCard.addEventListener("click", onClick);
+                const showCard = document.createElement('div');
+                showCard.classList.add('show-card');
+                showCard.addEventListener("click", onClick);
 
-    showCard.appendChild(dateLabel);
-    showCard.appendChild(date);
-    showCard.appendChild(venueLabel);
-    showCard.appendChild(place);
-    showCard.appendChild(locationLabel);
-    showCard.appendChild(location);
-    showCard.appendChild(button);
+                showCard.appendChild(dateLabel);
+                showCard.appendChild(date);
+                showCard.appendChild(venueLabel);
+                showCard.appendChild(place);
+                showCard.appendChild(locationLabel);
+                showCard.appendChild(location);
+                showCard.appendChild(button);
 
-    showContainer.appendChild(showCard);
-})
-}).catch(error => {
-    console.log(error);
-  })
+                showContainer.appendChild(showCard);
+            })
+        }).catch(error => {
+            console.log(error);
+        })
 }
 
 // Clicking on an individual row "selected" or "active"
 
-const elements = document.querySelectorAll('show-card'); 
+const elements = document.querySelectorAll('show-card');
 
 const onClick = (event) => {
     const clickedElement = event.currentTarget;
-    clickedElement.classList.add('show-card__active'); 
-
-    for (let i=0; i<elements.length; i++){
-    if (clickedElement !== elements ){
-    clickedElement.classList.remove('show-card__active');    
-    } else {
     clickedElement.classList.add('show-card__active');
-    } 
+
+    for (let i = 0; i < elements.length; i++) {
+        if (clickedElement !== elements) {
+            clickedElement.classList.remove('show-card__active');
+        } else {
+            clickedElement.classList.add('show-card__active');
+        }
     }
-  }
+}
 
-//   const showCards = document.querySelectorAll('show-card');
-
-//   for (let i=0; i < showCards.length; i++) {
-//     showCards[i].addEventListener("click",() => {
-//     showCards[i].classList.add('show-card__active');
-//      });
-//   }
-//   document.addEventListener('click', (event) => {
-//     for (let i=0; i<showCards.length; i++) {
-//     showCards[i].classList.remove('show-card__active');
-//     }
-//   });
-  
- 
-    displayShows();
+displayShows();
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,84 +3,85 @@ const apiKey = "33c092c8-f84f-497b-9e06-b1f2c281ccdf";
 // Creates HTML comment cards based off the above array
 
 const displayComment = () => {
+
     const commentContainer = document.querySelector('.comments__list');
     while (commentContainer.hasChildNodes()) { // wipe all comments and regenerate
         commentContainer.removeChild(commentContainer.lastChild);
-      }
+    }
 
     axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
-    .then(response => {
-        const commentsArray = response.data;
-        console.log(commentsArray);
-    
-    for (let i = commentsArray.length - 1; i >= 0; i--) {
-        const name = createElement('div',['comment__author'], commentsArray[i].name);
-        const newDate = new Date(commentsArray[i].timestamp);
-        const month = newDate.getMonth() + 1;
-        const day = newDate.getDate();
-        const year = newDate.getFullYear();
-        const date = month +  '/' + day + '/' + year; //Add all the month day year and append it
-        const timestamp = createElement('h3',['comment__timestamp'], date);
-        const comment = createElement('p',['comment__text'], commentsArray[i].comment);
-        const img = createElement('img',['comment__img'], '', commentsArray[i].img);
+        .then(response => {
+            const commentsArray = response.data;
+            console.log(commentsArray);
 
-        const commentCard = createElement('div', ['comment']);
+            for (let i = commentsArray.length - 1; i >= 0; i--) {
+                const name = createElement('div', ['comment__author'], commentsArray[i].name);
+                const newDate = new Date(commentsArray[i].timestamp);
+                const month = newDate.getMonth() + 1;
+                const day = newDate.getDate();
+                const year = newDate.getFullYear();
+                const date = month + '/' + day + '/' + year; //Add all the month day year and append it
+                const timestamp = createElement('h3', ['comment__timestamp'], date);
+                const comment = createElement('p', ['comment__text'], commentsArray[i].comment);
+                const img = createElement('img', ['comment__img'], '', commentsArray[i].img);
 
-        commentCard.appendChild(img);
-        commentCard.appendChild(name);
-        commentCard.appendChild(timestamp);
-        commentCard.appendChild(comment);
+                const commentCard = createElement('div', ['comment']);
 
-        commentContainer.appendChild(commentCard);
-    }
-})
+                commentCard.appendChild(img);
+                commentCard.appendChild(name);
+                commentCard.appendChild(timestamp);
+                commentCard.appendChild(comment);
+
+                commentContainer.appendChild(commentCard);
+            }
+        })
 }
 
 //  Creates HTML element with classname, content and src
 
 const createElement = (elementType = 'div', classes = [], content = '', src = '') => {
     let element = elementType === 'img' && !src
-      ? document.createElement('div') // If img src is empty then create a div with same class (grey background)
-      : document.createElement(elementType);
-  
+        ? document.createElement('div') // If img src is empty then create a div with same class (grey background)
+        : document.createElement(elementType);
+
     classes.forEach(className => {
-      element.classList.add(className);
+        element.classList.add(className);
     });
-  
+
     if (elementType === 'img' && src) {
-      element.setAttribute('src', src);
+        element.setAttribute('src', src);
     } else {
-      element.innerText = content;
+        element.innerText = content;
     }
-  
+
     return element;
-  }
+}
 
 displayComment();
 
 const commentForm = document.querySelector('.com-form');
 commentForm.addEventListener('submit', e => {
-      e.preventDefault(); // prevent Refresh
+    e.preventDefault(); // prevent Refresh
 
-axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`,
+    axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`,
         {
-          name: e.target.name.value,
-          comment: e.target.comment.value
+            name: e.target.name.value,
+            comment: e.target.comment.value
         }
-      )
-      .then(response => {
-        displayComment();
-      })
-      .catch(error => {
-        console.log(error);
-      })
+    )
+        .then(response => {
+            displayComment();
+        })
+        .catch(error => {
+            console.log(error);
+        })
 
-// Clear the comment after posting
+    // Clear the comment after posting
 
-    e.target.comment.value = ''; 
-    
-// Clear the name after posting
+    e.target.comment.value = '';
 
-    e.target.name.value = ''; 
-    
+    // Clear the name after posting
+
+    e.target.name.value = '';
+
 });
